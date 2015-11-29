2e89c2e8ae5f890b5b49d976ae89f3b3a779daa1
reis <rivvel@gmail.com>
2015-11-29 23:42:42 +0100

    add a rails API

---
+++ api/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+RatcatchrApi::Application.config.secret_token = '52acc4a59063a18ed4f3a135fb84310585374bc499034247930b6faef617a91a110cf7eb1e7cfb89443c61825080dd162ee56384bbc34ff474aab46564f89808'
